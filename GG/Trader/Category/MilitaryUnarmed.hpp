13f5c32d81fb7f433147ecd927f289a404cd18ab
Gr8 <gr8_boi52@yahoo.com>
2015-02-10 21:33:32 -0800

    BMP Ambulance Price
    Changed Price to a significant lower amount

--- GG/Trader/Category/MilitaryUnarmed.hpp
+++ GG/Trader/Category/MilitaryUnarmed.hpp
@@ -8,4 +8,4 @@ class GAZ_Vodnik_MedEvac { type = "trade_any_vehicle"; buy[] = {10000,"Coins"}; 
 class HMMWV_DZ { type = "trade_any_vehicle"; buy[] = {4000,"Coins"}; sell[] = {2000,"Coins"}; }; 
 class LandRover_CZ_EP1 { type = "trade_any_vehicle"; buy[] = {2000,"Coins"}; sell[] = {1000,"Coins"}; }; 
 class LandRover_TK_CIV_EP1 { type = "trade_any_vehicle"; buy[] = {2000,"Coins"}; sell[] = {1000,"Coins"}; }; 
-class BMP2_Ambul_CDF { type = "trade_any_vehicle"; buy[] = {80000,"Coins"}; sell[] = {10000,"Coins"}; }; }; 
+class BMP2_Ambul_CDF { type = "trade_any_vehicle"; buy[] = {40000,"Coins"}; sell[] = {30000,"Coins"}; }; }; 
