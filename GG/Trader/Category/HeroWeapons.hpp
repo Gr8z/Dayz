d2d8114ab5e818a95c51d892b3e2bd8630b93a00
GhostGamerz <Psychotic_closure@hotmail.com>
2015-02-18 19:09:58 -0800

    Lapua Price
    bandit/hero buy/sell price

--- GG/Trader/Category/HeroWeapons.hpp
+++ GG/Trader/Category/HeroWeapons.hpp
@@ -18,7 +18,7 @@ class G36_C_SD_camo { type = "trade_weapons"; buy[] = {1000,"Coins"}; sell[] = {
 class M4A1_AIM_SD_camo { type = "trade_weapons"; buy[] = {1000,"Coins"}; sell[] = {500,"Coins"}; }; 
 class FN_FAL_ANPVS4 { type = "trade_weapons"; buy[] = {2000,"Coins"}; sell[] = {1000,"Coins"}; }; 
 class SCAR_H_LNG_Sniper_SD { type = "trade_weapons"; buy[] = {2000,"Coins"}; sell[] = {1000,"Coins"}; }; 
-class BAF_LRR_scoped { type = "trade_weapons"; buy[] = {30000,"Coins"}; sell[] = {2000,"Coins"}; }; 
+class BAF_LRR_scoped { type = "trade_weapons"; buy[] = {45000,"Coins"}; sell[] = {15000,"Coins"}; }; 
 class FN_FAL { type = "trade_weapons"; buy[] = {1000,"Coins"}; sell[] = {500,"Coins"}; }; 
 class Mk_48_DZ { type = "trade_weapons"; buy[] = {2000,"Coins"}; sell[] = {1000,"Coins"}; }; 
 class M240_DZ { type = "trade_weapons"; buy[] = {1000,"Coins"}; sell[] = {500,"Coins"}; };  
