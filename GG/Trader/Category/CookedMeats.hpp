fdf6d2af7806fb9262ef627127e73aa7c009bb2d
Phantomz98 <michael.lawson@go.mrapats.com>
2015-03-01 21:06:53 -0600

    Fish Sell Price Fix
    Changed Fish Sell Prices, Trout = 200-250, SeaBass = 300-500,
    Tuna = 1000 - 1250.

--- GG/Trader/Category/CookedMeats.hpp
+++ GG/Trader/Category/CookedMeats.hpp
@@ -5,6 +5,6 @@ class FoodbeefCooked { type = "trade_items"; buy[] = {40,"Coins"}; sell[] = {20,
 class FoodchickenCooked { type = "trade_items"; buy[] = {40,"Coins"}; sell[] = {20,"Coins"}; }; 
 class FoodmuttonCooked { type = "trade_items"; buy[] = {40,"Coins"}; sell[] = {20,"Coins"}; }; 
 class FoodrabbitCooked { type = "trade_items"; buy[] = {20,"Coins"}; sell[] = {10,"Coins"}; }; 
-class ItemTroutCooked { type = "trade_items"; buy[] = {2000,"Coins"}; sell[] = {200,"Coins"}; }; 
-class ItemSeaBassCooked { type = "trade_items"; buy[] = {3000,"Coins"}; sell[] = {300,"Coins"}; }; 
-class ItemTunaCooked { type = "trade_items"; buy[] = {6000,"Coins"}; sell[] = {1000,"Coins"}; }; }; 
+class ItemTroutCooked { type = "trade_items"; buy[] = {2000,"Coins"}; sell[] = {250,"Coins"}; }; 
+class ItemSeaBassCooked { type = "trade_items"; buy[] = {3000,"Coins"}; sell[] = {500,"Coins"}; }; 
+class ItemTunaCooked { type = "trade_items"; buy[] = {6000,"Coins"}; sell[] = {1250,"Coins"}; }; }; 
