33506586303aea57b562beef4ab0cc588e33ba0e
Dedicated Machine <gr8_boi52@yahoo.com>
2015-03-16 22:48:22 -0800

    Change sql dir link

--- SQL_SCRIPTS/server_2.bat
+++ SQL_SCRIPTS/server_2.bat
@@ -10,5 +10,5 @@ echo.
 echo RUNNING SQL Cleanup Script...
 echo.
 echo.
-D:\MySQL\bin\mysql.exe -u %mysqluser% -p%mysqlpassword% %mysqlschema%  < "C:\Dayz\SQL_SCRIPTS\server_2.sql"
+E:\MySQL\bin\mysql.exe -u %mysqluser% -p%mysqlpassword% %mysqlschema%  < "C:\Dayz\SQL_SCRIPTS\server_2.sql"
 exit
