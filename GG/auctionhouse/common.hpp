90827ec9dbf5f782fad875c8b2588538d8c55048
GhostGamerz <Psychotic_closure@hotmail.com>
2015-04-04 16:22:29 -0400

    Removed Already defined Class

--- GG/auctionhouse/common.hpp
+++ GG/auctionhouse/common.hpp
@@ -1,25 +1,3 @@
-class Life_RscText {
-	x = 0;
-	y = 0;
-	h = 0.037;
-	w = 0.3;
-	type = 0;
-	style = 0;
-	shadow = 1;
-	colorShadow[] = {0, 0, 0, 0.5};
-	font = "Zeppelin32";
-	SizeEx = "(			(			(			((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 1)";
-	text = "";
-	colorText[] = {1, 1, 1, 1.0};
-	colorBackground[] = {0, 0, 0, 0};
-	linespacing = 1;
-	tooltipColorText[] = {1,1,1,1};
-	tooltipColorBox[] = {1,1,1,1};
-	tooltipColorShade[] = {0,0,0,0.65};
-};
-
-
-
 class Life_RscShortcutButton {
 	idc = -1;
 	style = 0;
