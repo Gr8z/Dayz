0edf45671dad9559cdc3e5bc9c7227d3bf217362
TempSniper <bethandmark@nycap.rr.com>
2015-05-25 11:11:24 -0400

    Fix for pricces
    Duh me

--- GG/Trader/Category/OWDMR.hpp
+++ GG/Trader/Category/OWDMR.hpp
@@ -11,9 +11,9 @@ class RH_sc2aim { type = "trade_weapons"; buy[] = {1100,"Coins"}; sell[] = {600,
 class RH_sc2eot { type = "trade_weapons"; buy[] = {1200,"Coins"}; sell[] = {800,"Coins"}; }; 
 class RH_sc2shd { type = "trade_weapons"; buy[] = {1300,"Coins"}; sell[] = {900,"Coins"}; }; 
 class RH_sc2sp { type = "trade_weapons"; buy[] = {1400,"Coins"}; sell[] = {700,"Coins"}; }; 
-class vil_SR25 { type = "trade_weapons"; buy[] = {2000,"Coins"}; sell[] = {8000,"Coins"}; }; 
+class vil_SR25 { type = "trade_weapons"; buy[] = {2000,"Coins"}; sell[] = {800,"Coins"}; }; 
 class vil_SR25SD { type = "trade_weapons"; buy[] = {3000,"Coins"}; sell[] = {900,"Coins"}; }; 
-class vil_M110 { type = "trade_weapons"; buy[] = {2000,"Coins"}; sell[] = {8000,"Coins"}; }; 
+class vil_M110 { type = "trade_weapons"; buy[] = {2000,"Coins"}; sell[] = {800,"Coins"}; }; 
 class vil_M110sd { type = "trade_weapons"; buy[] = {3000,"Coins"}; sell[] = {1500,"Coins"}; }; 
 class vil_M21 { type = "trade_weapons"; buy[] = {2000,"Coins"}; sell[] = {800,"Coins"}; }; 
 class vil_M21G { type = "trade_weapons"; buy[] = {2000,"Coins"}; sell[] = {800,"Coins"}; }; 
