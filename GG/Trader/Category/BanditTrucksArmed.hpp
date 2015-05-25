e5fa1358c27fdd187205309de9b32a30a63c4c1c
TempSniper <bethandmark@nycap.rr.com>
2015-05-25 11:27:59 -0400

    fix
    double whammy

--- GG/Trader/Category/BanditTrucksArmed.hpp
+++ GG/Trader/Category/BanditTrucksArmed.hpp
@@ -4,4 +4,4 @@ class Pickup_PK_GUE_DZE { type = "trade_any_vehicle"; buy[] = {1000,"Coins"}; se
 class Pickup_PK_INS_DZE { type = "trade_any_vehicle"; buy[] = {1000,"Coins"}; sell[] = {500,"Coins"}; }; 
 class ArmoredSUV_PMC_DZE { type = "trade_any_vehicle"; buy[] = {20000,"Coins"}; sell[] = {5000,"Coins"}; }; 
 class Pickup_PK_TK_GUE_EP1_DZE { type = "trade_any_vehicle"; buy[] = {800,"Coins"}; sell[] = {400,"Coins"}; }; 
-class Offroad_DSHKM_Gue_DZE { type = "trade_any_vehicle"; buy[] = {2000,"Coins"}; sell[] = {7000,"Coins"}; }; }; 
+class Offroad_DSHKM_Gue_DZE { type = "trade_any_vehicle"; buy[] = {2000,"Coins"}; sell[] = {700,"Coins"}; }; }; 
