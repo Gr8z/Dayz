8c7a3f1d31e65da597bc267ef01614ada66fde7c
GhostGamerz <Psychotic_closure@hotmail.com>
2015-05-29 23:23:40 -0400

    Revert "Price for Static Guns"
    This reverts commit 379a8d3f149a3c0d38a88d905055066c6646e09a.

--- GG/Trader/Category/Static.hpp
+++ GG/Trader/Category/Static.hpp
@@ -1,9 +1,9 @@
 
 class Category_1022 { 
-	class DSHKM_TK_GUE_BAG_EP1 {type = "trade_backpacks";buy[] = {50000,"Coins"};sell[] = {20000,"Coins"};}; 
-	class M2StaticMG_US_Bag_EP1 {type = "trade_backpacks";buy[] = {60000,"Coins"};sell[] = {20000,"Coins"};}; 
-	class KORD_UN_Bag_EP1 {type = "trade_backpacks";buy[] = {70000,"Coins"};sell[] = {20000,"Coins"};}; 
-	class AGS_UN_Bag_EP1 {type = "trade_backpacks";buy[] = {80000,"Coins"};sell[] = {20000,"Coins"};}; 
-	class MK19_TriPod_US_Bag_EP1 {type = "trade_backpacks";buy[] = {90000,"Coins"};sell[] = {20000,"Coins"};}; 
-	class BAF_GMG_ACOG_minitripod_bag {type = "trade_backpacks";buy[] = {100000,"Coins"};sell[] = {30000,"Coins"};}; 
+	class DSHKM_TK_GUE_BAG_EP1 {type = "trade_backpacks";buy[] = {20000,"Coins"};sell[] = {500,"Coins"};}; 
+	class M2StaticMG_US_Bag_EP1 {type = "trade_backpacks";buy[] = {10000,"Coins"};sell[] = {500,"Coins"};}; 
+	class KORD_UN_Bag_EP1 {type = "trade_backpacks";buy[] = {70000,"Coins"};sell[] = {3000,"Coins"};}; 
+	class AGS_UN_Bag_EP1 {type = "trade_backpacks";buy[] = {70000,"Coins"};sell[] = {3000,"Coins"};}; 
+	class MK19_TriPod_US_Bag_EP1 {type = "trade_backpacks";buy[] = {70000,"Coins"};sell[] = {3000,"Coins"};}; 
+	class BAF_GMG_ACOG_minitripod_bag {type = "trade_backpacks";buy[] = {70000,"Coins"};sell[] = {3000,"Coins"};}; 
 };
