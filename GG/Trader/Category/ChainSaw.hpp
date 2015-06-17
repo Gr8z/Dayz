a6c584b4f3ddc2ab88bea853664102d139825146
TempSniper <bethandmark@nycap.rr.com>
2015-06-17 10:58:08 -0400

    Chainsaw V1.1
    15% sell 10% buy

--- GG/Trader/Category/ChainSaw.hpp
+++ GG/Trader/Category/ChainSaw.hpp
@@ -1,7 +1,7 @@
 class Category_500 { 
-class ChainSaw { type = "trade_weapons"; buy[] = {20000,"Coins"}; sell[] = {5000,"Coins"}; }; 
-class ChainSawR { type = "trade_weapons"; buy[] = {20000,"Coins"}; sell[] = {5000,"Coins"}; }; 
-class ChainSawP { type = "trade_weapons"; buy[] = {20000,"Coins"}; sell[] = {5000,"Coins"}; }; 
-class ChainSawG { type = "trade_weapons"; buy[] = {20000,"Coins"}; sell[] = {5000,"Coins"}; }; 
-class ChainSawB { type = "trade_weapons"; buy[] = {20000,"Coins"}; sell[] = {5000,"Coins"}; }; 
+class ChainSaw { type = "trade_weapons"; buy[] = {18000,"Coins"}; sell[] = {5750,"Coins"}; }; 
+class ChainSawR { type = "trade_weapons"; buy[] = {18000,"Coins"}; sell[] = {5750,"Coins"}; }; 
+class ChainSawP { type = "trade_weapons"; buy[] = {18000,"Coins"}; sell[] = {5750,"Coins"}; }; 
+class ChainSawG { type = "trade_weapons"; buy[] = {18000,"Coins"}; sell[] = {5750,"Coins"}; }; 
+class ChainSawB { type = "trade_weapons"; buy[] = {18000,"Coins"}; sell[] = {5750,"Coins"}; }; 
 };
