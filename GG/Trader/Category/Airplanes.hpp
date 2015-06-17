27e6bb8ffab148711b7179e5150777b417ca0e3b
TempSniper <bethandmark@nycap.rr.com>
2015-06-17 09:37:52 -0400

    Price Version 1.0
    15% Sell, 10% Buy

--- GG/Trader/Category/Airplanes.hpp
+++ GG/Trader/Category/Airplanes.hpp
@@ -1,12 +1,12 @@
 
 class Category_517 { 
-class AN2_DZ { type = "trade_any_vehicle"; buy[] = {4000,"Coins"}; sell[] = {2000,"Coins"}; }; 
-class C130J_US_EP1_DZ { type = "trade_any_vehicle"; buy[] = {40000,"Coins"}; sell[] = {20000,"Coins"}; }; 
-class MV22_DZ { type = "trade_any_vehicle"; buy[] = {50000,"Coins"}; sell[] = {20000,"Coins"}; }; 
-class GNT_C185U { type = "trade_any_vehicle"; buy[] = {24000,"Coins"}; sell[] = {2000,"Coins"}; }; 
-class GNT_C185 { type = "trade_any_vehicle"; buy[] = {24000,"Coins"}; sell[] = {2000,"Coins"}; }; 
-class GNT_C185R { type = "trade_any_vehicle"; buy[] = {24000,"Coins"}; sell[] = {2000,"Coins"}; }; 
-class GNT_C185C { type = "trade_any_vehicle"; buy[] = {24000,"Coins"}; sell[] = {2000,"Coins"}; }; 
-class CYBP_Camel_us { type = "trade_any_vehicle"; buy[] = {8000,"Coins"}; sell[] = {4000,"Coins"}; }; 
-class CYBP_Camel_rus { type = "trade_any_vehicle"; buy[] = {8000,"Coins"}; sell[] = {4000,"Coins"}; }; 
-class CYBP_Camel_civ { type = "trade_any_vehicle"; buy[] = {8000,"Coins"}; sell[] = {4000,"Coins"}; };  }; 
+class AN2_DZ { type = "trade_any_vehicle"; buy[] = {3600,"Coins"}; sell[] = {2300,"Coins"}; }; 
+class C130J_US_EP1_DZ { type = "trade_any_vehicle"; buy[] = {36000,"Coins"}; sell[] = {23000,"Coins"}; }; 
+class MV22_DZ { type = "trade_any_vehicle"; buy[] = {45000,"Coins"}; sell[] = {20000,"Coins"}; }; 
+class GNT_C185U { type = "trade_any_vehicle"; buy[] = {21600,"Coins"}; sell[] = {2300,"Coins"}; }; 
+class GNT_C185 { type = "trade_any_vehicle"; buy[] = {21600,"Coins"}; sell[] = {2300,"Coins"}; }; 
+class GNT_C185R { type = "trade_any_vehicle"; buy[] = {21600,"Coins"}; sell[] = {2300,"Coins"}; }; 
+class GNT_C185C { type = "trade_any_vehicle"; buy[] = {21600,"Coins"}; sell[] = {2300,"Coins"}; }; 
+class CYBP_Camel_us { type = "trade_any_vehicle"; buy[] = {7200,"Coins"}; sell[] = {4600,"Coins"}; }; 
+class CYBP_Camel_rus { type = "trade_any_vehicle"; buy[] = {7200,"Coins"}; sell[] = {4600,"Coins"}; }; 
+class CYBP_Camel_civ { type = "trade_any_vehicle"; buy[] = {7200,"Coins"}; sell[] = {4600,"Coins"}; };  }; 
