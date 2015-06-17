ec8738b2edbc6615e71defa2a671617b27a6d8ba
TempSniper <bethandmark@nycap.rr.com>
2015-06-17 10:46:10 -0400

    Building Supplies
    15% Sell 10% buy

--- GG/Trader/Category/BuildingSupplies.hpp
+++ GG/Trader/Category/BuildingSupplies.hpp
@@ -2,19 +2,19 @@
 class Category_678 { 
 class ItemSandbag { type = "trade_items"; buy[] = {400,"Coins"}; sell[] = {200,"Coins"}; }; 
 class ItemTankTrap { type = "trade_items"; buy[] = {20,"Coins"}; sell[] = {10,"Coins"}; }; 
-class CinderBlocks { type = "trade_items"; buy[] = {1000,"Coins"}; sell[] = {400,"Coins"}; }; 
-class MortarBucket { type = "trade_items"; buy[] = {1000,"Coins"}; sell[] = {400,"Coins"}; }; 
+class CinderBlocks { type = "trade_items"; buy[] = {900,"Coins"}; sell[] = {460,"Coins"}; }; 
+class MortarBucket { type = "trade_items"; buy[] = {900,"Coins"}; sell[] = {460,"Coins"}; }; 
 class ItemTentOld { type = "trade_items"; buy[] = {40,"Coins"}; sell[] = {20,"Coins"}; }; 
 class ItemWire { type = "trade_items"; buy[] = {6,"Coins"}; sell[] = {3,"Coins"}; }; 
 class ItemPole { type = "trade_items"; buy[] = {20,"Coins"}; sell[] = {10,"Coins"}; }; 
-class 30m_plot_kit { type = "trade_items"; buy[] = {12000,"Coins"}; sell[] = {3000,"Coins"}; }; 
-class ItemVault { type = "trade_items"; buy[] = {10000,"Coins"}; sell[] = {5000,"Coins"}; }; 
-class itemcombolock { type = "trade_items"; buy[] = {5000,"Coins"}; sell[] = {2000,"Coins"}; };  
+class 30m_plot_kit { type = "trade_items"; buy[] = {10800,"Coins"}; sell[] = {3450,"Coins"}; }; 
+class ItemVault { type = "trade_items"; buy[] = {9000,"Coins"}; sell[] = {5750,"Coins"}; }; 
+class itemcombolock { type = "trade_items"; buy[] = {4500,"Coins"}; sell[] = {2300,"Coins"}; };  
 class ItemTentDomed { type = "trade_items"; buy[] = {60,"Coins"}; sell[] = {30,"Coins"}; }; 
 class ItemTentDomed2 { type = "trade_items"; buy[] = {60,"Coins"}; sell[] = {30,"Coins"}; }; 
 class ItemLightBulb { type = "trade_items"; buy[] = {20,"Coins"}; sell[] = {10,"Coins"}; }; 
 class Laserbatteries { type = "trade_items"; buy[] = {40,"Coins"}; sell[] = {20,"Coins"}; }; 
-class ItemGenerator { type = "trade_items"; buy[] = {600,"Coins"}; sell[] = {300,"Coins"}; }; 
-class fuel_pump_kit { type = "trade_items"; buy[] = {8000,"Coins"}; sell[] = {1000,"Coins"}; };
+class ItemGenerator { type = "trade_items"; buy[] = {540,"Coins"}; sell[] = {345,"Coins"}; }; 
+class fuel_pump_kit { type = "trade_items"; buy[] = {7200,"Coins"}; sell[] = {1150,"Coins"}; };
 
 }; 
