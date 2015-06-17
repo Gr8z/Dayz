11028f47496afefb3ddbe9447cf9de9091fb55f3
TempSniper <bethandmark@nycap.rr.com>
2015-06-17 10:04:40 -0400

    Bandit V.1.1
    15% sell 10% buy

--- GG/Trader/Category/BanditMilitaryArmed.hpp
+++ GG/Trader/Category/BanditMilitaryArmed.hpp
@@ -1,16 +1,16 @@
 
 class Category_569 { 
-class HMMWV_M998A2_SOV_DES_EP1_DZE {type = "trade_any_vehicle";buy[] = {30000,"Coins"};sell[] = {4000,"Coins"};}; 
-class HMMWV_M1151_M2_CZ_DES_EP1_DZE {type = "trade_any_vehicle";buy[] = {28000,"Coins"};sell[] = {5000,"Coins"};};
-class HMMWV_m998_crows_M2_DES_EP1 {type = "trade_any_vehicle";buy[] = {200000,"Coins"};sell[] = {37500,"Coins"};}; 
-class BTR40_MG_TK_GUE_EP1 {type = "trade_any_vehicle";buy[] = {25000,"Coins"};sell[] = {7500,"Coins"};}; 
-class LandRover_MG_TK_EP1_DZE {type = "trade_any_vehicle";buy[] = {6000,"Coins"};sell[] = {1500,"Coins"};}; 
-class UAZ_MG_TK_EP1_DZE {type = "trade_any_vehicle";buy[] = {6000,"Coins"};sell[] = {1500,"Coins"};}; 
-class BAF_Jackal2_L2A1_W {type = "trade_any_vehicle";buy[] = {40000,"Coins"};sell[] = {10000,"Coins"};};
-class BAF_Jackal2_GMG_W {type = "trade_any_vehicle";buy[] = {50000,"Coins"};sell[] = {12500,"coins"};};
-class BRDM2_TK_EP1 {type = "trade_any_vehicle";buy[] = {150000,"Coins"};sell[] = {37500,"Coins"};}; 
-class BTR90_HQ {type = "trade_any_vehicle";buy[] = {70000,"Coins"};sell[] = {25000,"Coins"};}; 
-class LAV25_HQ {type = "trade_any_vehicle";buy[] = {70000,"Coins"};sell[] = {25000,"Coins"};}; 
-class M113_TK_EP1 {type = "trade_any_vehicle";buy[] = {90000,"Coins"};sell[] = {25000,"Coins"};}; 
-class M113_UN_EP1 {type = "trade_any_vehicle";buy[] = {90000,"Coins"};sell[] = {25000,"Coins"};}; 
+class HMMWV_M998A2_SOV_DES_EP1_DZE {type = "trade_any_vehicle";buy[] = {27000,"Coins"};sell[] = {4600,"Coins"};}; 
+class HMMWV_M1151_M2_CZ_DES_EP1_DZE {type = "trade_any_vehicle";buy[] = {25400,"Coins"};sell[] = {5750,"Coins"};};
+class HMMWV_m998_crows_M2_DES_EP1 {type = "trade_any_vehicle";buy[] = {180000,"Coins"};sell[] = {43000,"Coins"};}; 
+class BTR40_MG_TK_GUE_EP1 {type = "trade_any_vehicle";buy[] = {22500,"Coins"};sell[] = {8625,"Coins"};}; 
+class LandRover_MG_TK_EP1_DZE {type = "trade_any_vehicle";buy[] = {3400,"Coins"};sell[] = {1725,"Coins"};}; 
+class UAZ_MG_TK_EP1_DZE {type = "trade_any_vehicle";buy[] = {5400,"Coins"};sell[] = {1725,"Coins"};}; 
+class BAF_Jackal2_L2A1_W {type = "trade_any_vehicle";buy[] = {36000,"Coins"};sell[] = {11500,"Coins"};};
+class BAF_Jackal2_GMG_W {type = "trade_any_vehicle";buy[] = {45000,"Coins"};sell[] = {14375,"coins"};};
+class BRDM2_TK_EP1 {type = "trade_any_vehicle";buy[] = {135000,"Coins"};sell[] = {43000,"Coins"};}; 
+class BTR90_HQ {type = "trade_any_vehicle";buy[] = {63000,"Coins"};sell[] = {28750,"Coins"};}; 
+class LAV25_HQ {type = "trade_any_vehicle";buy[] = {63000,"Coins"};sell[] = {28750,"Coins"};}; 
+class M113_TK_EP1 {type = "trade_any_vehicle";buy[] = {81000,"Coins"};sell[] = {28750,"Coins"};}; 
+class M113_UN_EP1 {type = "trade_any_vehicle";buy[] = {81000,"Coins"};sell[] = {28750,"Coins"};}; 
 };
