a0c571aaf13dd4984f5758ae7efb0d44ab78a7b0
TempSniper <bethandmark@nycap.rr.com>
2015-06-17 10:27:57 -0400

    Bandittrucks V.1.1
    15% sell 10% buy

--- GG/Trader/Category/BanditTrucksArmed.hpp
+++ GG/Trader/Category/BanditTrucksArmed.hpp
@@ -1,7 +1,7 @@
 
 class Category_534 { 
-class Pickup_PK_GUE_DZE { type = "trade_any_vehicle"; buy[] = {1000,"Coins"}; sell[] = {500,"Coins"}; }; 
-class Pickup_PK_INS_DZE { type = "trade_any_vehicle"; buy[] = {1000,"Coins"}; sell[] = {500,"Coins"}; }; 
-class ArmoredSUV_PMC_DZE { type = "trade_any_vehicle"; buy[] = {20000,"Coins"}; sell[] = {5000,"Coins"}; }; 
-class Pickup_PK_TK_GUE_EP1_DZE { type = "trade_any_vehicle"; buy[] = {800,"Coins"}; sell[] = {400,"Coins"}; }; 
-class Offroad_DSHKM_Gue_DZE { type = "trade_any_vehicle"; buy[] = {2000,"Coins"}; sell[] = {700,"Coins"}; }; }; 
+class Pickup_PK_GUE_DZE { type = "trade_any_vehicle"; buy[] = {900,"Coins"}; sell[] = {575,"Coins"}; }; 
+class Pickup_PK_INS_DZE { type = "trade_any_vehicle"; buy[] = {900,"Coins"}; sell[] = {575,"Coins"}; }; 
+class ArmoredSUV_PMC_DZE { type = "trade_any_vehicle"; buy[] = {18000,"Coins"}; sell[] = {5750,"Coins"}; }; 
+class Pickup_PK_TK_GUE_EP1_DZE { type = "trade_any_vehicle"; buy[] = {720,"Coins"}; sell[] = {460,"Coins"}; }; 
+class Offroad_DSHKM_Gue_DZE { type = "trade_any_vehicle"; buy[] = {1800,"Coins"}; sell[] = {805,"Coins"}; }; }; 
