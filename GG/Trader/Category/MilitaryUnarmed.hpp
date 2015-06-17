bd961fd4eada750715a7f19aa2276903b74bf484
TempSniper <bethandmark@nycap.rr.com>
2015-06-17 11:40:48 -0400

    MilitaryUnarmed
    15%sell 10% buy

--- GG/Trader/Category/MilitaryUnarmed.hpp
+++ GG/Trader/Category/MilitaryUnarmed.hpp
@@ -1,11 +1,11 @@
 
 class Category_491 { 
-class HMMWV_M1035_DES_EP1 { type = "trade_any_vehicle"; buy[] = {4000,"Coins"}; sell[] = {1000,"Coins"}; }; 
-class HMMWV_Ambulance { type = "trade_any_vehicle"; buy[] = {4000,"Coins"}; sell[] = {1000,"Coins"}; }; 
-class HMMWV_Ambulance_CZ_DES_EP1 { type = "trade_any_vehicle"; buy[] = {4000,"Coins"}; sell[] = {1000,"Coins"}; }; 
-class HMMWV_DES_EP1 { type = "trade_any_vehicle"; buy[] = {4000,"Coins"}; sell[] = {1000,"Coins"}; }; 
-class GAZ_Vodnik_MedEvac { type = "trade_any_vehicle"; buy[] = {10000,"Coins"}; sell[] = {1500,"Coins"}; }; 
-class HMMWV_DZ { type = "trade_any_vehicle"; buy[] = {4000,"Coins"}; sell[] = {1000,"Coins"}; }; 
-class LandRover_CZ_EP1 { type = "trade_any_vehicle"; buy[] = {2000,"Coins"}; sell[] = {1000,"Coins"}; }; 
-class LandRover_TK_CIV_EP1 { type = "trade_any_vehicle"; buy[] = {2000,"Coins"}; sell[] = {1000,"Coins"}; }; 
-class BMP2_Ambul_CDF { type = "trade_any_vehicle"; buy[] = {40000,"Coins"}; sell[] = {18000,"Coins"}; }; }; 
+class HMMWV_M1035_DES_EP1 { type = "trade_any_vehicle"; buy[] = {3600,"Coins"}; sell[] = {1150,"Coins"}; }; 
+class HMMWV_Ambulance { type = "trade_any_vehicle"; buy[] = {3600,"Coins"}; sell[] = {1150,"Coins"}; }; 
+class HMMWV_Ambulance_CZ_DES_EP1 { type = "trade_any_vehicle"; buy[] = {3600,"Coins"}; sell[] = {1150,"Coins"}; }; 
+class HMMWV_DES_EP1 { type = "trade_any_vehicle"; buy[] = {3600,"Coins"}; sell[] = {1150,"Coins"}; }; 
+class GAZ_Vodnik_MedEvac { type = "trade_any_vehicle"; buy[] = {9000,"Coins"}; sell[] = {1725,"Coins"}; }; 
+class HMMWV_DZ { type = "trade_any_vehicle"; buy[] = {3600,"Coins"}; sell[] = {1150,"Coins"}; }; 
+class LandRover_CZ_EP1 { type = "trade_any_vehicle"; buy[] = {1800,"Coins"}; sell[] = {1150,"Coins"}; }; 
+class LandRover_TK_CIV_EP1 { type = "trade_any_vehicle"; buy[] = {1800,"Coins"}; sell[] = {1150,"Coins"}; }; 
+class BMP2_Ambul_CDF { type = "trade_any_vehicle"; buy[] = {36000,"Coins"}; sell[] = {19000,"Coins"}; }; }; 
