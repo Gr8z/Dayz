f73d2eeed3a0833308ccad7d2f6c099cbd7e7e31
TempSniper <bethandmark@nycap.rr.com>
2015-06-23 08:51:02 -0400

    Drugs Price Change
    Drug War is over

--- GG/Trader/Category/drugs.hpp
+++ GG/Trader/Category/drugs.hpp
@@ -1,3 +1,3 @@
 
 class Category_1021 { 
-class ItemKiloHemp {type = "trade_items";buy[] = {4000,"Coins"};sell[] = {1500,"Coins"};}; }; 
+class ItemKiloHemp {type = "trade_items";buy[] = {4000,"Coins"};sell[] = {1000,"Coins"};}; }; 
