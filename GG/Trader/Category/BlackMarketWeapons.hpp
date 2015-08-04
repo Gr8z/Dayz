0168038366c4bea5a935c0ada0ef23fe9cbafc7c
GGBigEgg <jeremydupont17@outlook.com>
2015-08-04 13:46:58 -0500

    Moved m240 nest to black market

--- GG/Trader/Category/BlackMarketWeapons.hpp
+++ GG/Trader/Category/BlackMarketWeapons.hpp
@@ -1,5 +1,6 @@
 
-class Category_526 { 
+class Category_526 {
+class m240_nest_kit { type = "trade_items"; buy[] = {10000,"Coins"}; sell[] = {5000,"Coins"}; };
 class Saiga12K { type = "trade_weapons"; buy[] = {450,"Coins"}; sell[] = {345,"Coins"}; }; 
 class AKS_GOLD { type = "trade_weapons"; buy[] = {2700,"Coins"}; sell[] = {1725,"Coins"}; };
 class m8_compact { type = "trade_weapons"; buy[] = {360,"Coins"}; sell[] = {260,"Coins"}; }; 
@@ -25,5 +26,5 @@ class M4A1_HWS_GL_SD_Camo { type = "trade_weapons"; buy[] = {1800,"Coins"}; sell
 class KSVK_DZE { type = "trade_weapons"; buy[] = {27000,"Coins"}; sell[] = {5750,"Coins"}; }; 
 class RPG7V { type = "trade_weapons"; buy[] = {45000,"Coins"}; sell[] = {5750,"Coins"}; }; 
 class M79_EP1 { type = "trade_weapons"; buy[] = {22500,"Coins"}; sell[] = {4600,"Coins"}; }; 
-class M32_EP1 { type = "trade_weapons"; buy[] = {67500,"Coins"}; sell[] = {11500,"Coins"}; }; 
+class M32_EP1 { type = "trade_weapons"; buy[] = {67500,"Coins"}; sell[] = {11500,"Coins"}; };
 }; 
