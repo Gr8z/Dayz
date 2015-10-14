e58f5211a72093c8680be2ca629df5baf6a0b938
BigEgg <jeremydupont17@outlook.com>
2015-10-13 21:08:09 -0500

    Price Adjustments

--- GG/Trader/Category/TAmmunition.hpp
+++ GG/Trader/Category/TAmmunition.hpp
@@ -7,9 +7,9 @@ class FHQ_rem_7Rnd_338Lapua_MSR_NT_SD 	{type = "trade_items";			buy[] = {1000,"C
 class 10Rnd_127x99_m107 				{type = "trade_items";			buy[] = {3600,"Coins"};			sell[] = {1450,"Coins"};}; 
 class 5Rnd_127x108_KSVK 				{type = "trade_items";			buy[] = {100,"Coins"};			sell[] = {100,"Coins"};}; 
 class 20Rnd_762x51_DMR 					{type = "trade_items"; 			buy[] = {100,"Coins"}; 			sell[] = {100,"Coins"}; }; 
-class RPG18 							{type = "trade_items";			buy[] = {10000,"Coins"};		sell[] = {5000,"Coins"};}; 
-class Stinger 							{type = "trade_items";			buy[] = {10000,"Coins"};		sell[] = {5000,"Coins"};};
-class Javelin 							{type = "trade_items";			buy[] = {10000,"Coins"};		sell[] = {5000,"Coins"};};
+class RPG18 							{type = "trade_items";			buy[] = {20000,"Coins"};		sell[] = {5000,"Coins"};}; 
+class Stinger 							{type = "trade_items";			buy[] = {25000,"Coins"};		sell[] = {5000,"Coins"};};
+class Javelin 							{type = "trade_items";			buy[] = {25000,"Coins"};		sell[] = {5000,"Coins"};};
 class 2000Rnd_762x51_M134 				{type = "trade_items";			buy[] = {575,"Coins"};			sell[] = {250,"Coins"};}; 
 class 100Rnd_127x99_M2 					{type = "trade_items";			buy[] = {350,"Coins"};			sell[] = {350,"Coins"};};
 }; 
