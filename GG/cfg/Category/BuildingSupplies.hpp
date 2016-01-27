00b46c9bfdd0a9a237fec2a9d2a86ffe2643d526
BigEgg <jeremydupont17@outlook.com>
2016-01-26 22:10:41 -0600

    Adjusted Vault Price

--- GG/cfg/Category/BuildingSupplies.hpp
+++ GG/cfg/Category/BuildingSupplies.hpp
@@ -8,7 +8,7 @@ class ItemTentOld: d_tit {buy = 40;sell = 20;};
 class ItemWire: d_tit {buy = 6;sell = 3;}; 
 class ItemPole: d_tit {buy = 20;sell = 10;}; 
 class 30m_plot_kit: d_tit {buy = 10800;sell = 3450;}; 
-class ItemVault: d_tit {buy = 9000;sell = 5750;}; 
+class ItemVault: d_tit {buy = 9000;sell = 4500;}; 
 class itemcombolock: d_tit {buy = 4500;sell = 2300;};  
 class ItemTentDomed: d_tit {buy = 60;sell = 30;}; 
 class ItemTentDomed2: d_tit {buy = 60;sell = 30;}; 
