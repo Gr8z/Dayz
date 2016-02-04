7f15d714dc7e377a1035f6ecc22162f6365b0e6a
shahzad1900 <shazzy1900@gmail.com>
2016-02-04 15:28:46 +0000

    prices change for the SVD's

--- GG/cfg/Category/SniperRifle.hpp
+++ GG/cfg/Category/SniperRifle.hpp
@@ -1,12 +1,12 @@
 
 class Category_487 { 
-class SVD_CAMO: d_tit {buy = 1000;sell = 600;}; 
+class SVD_CAMO: d_tit {buy = 1000;sell = 500;}; 
 class M40A3: d_tit {buy = 1000;sell = 600;}; 
 class M14_EP1: d_tit {buy = 2000;sell = 800;}; 
 class huntingrifle: d_tit {buy = 200;sell = 100;}; 
 class M4SPR: d_tit {buy = 1000;sell = 600;}; 
-class SVD: d_tit {buy = 1000;sell = 600;}; 
-class SVD_des_EP1: d_tit {buy = 1000;sell = 600;}; 
+class SVD: d_tit {buy = 1000;sell = 500;}; 
+class SVD_des_EP1: d_tit {buy = 1000;sell = 500;}; 
 class M24: d_tit {buy = 1000;sell = 600;}; 
 class M24_des_EP1: d_tit {buy = 1000;sell = 600;}; 
 class vil_M24b: d_tit {buy = 1000;sell = 600;}; 
@@ -14,14 +14,14 @@ class gms_k98: d_tit {buy = 400;sell = 200;};
 class gms_k98_rg: d_tit {buy = 500;sell = 200;}; 
 class gms_k98_knife: d_tit {buy = 400;sell = 200;}; 
 class gms_k98zf39: d_tit {buy = 600;sell = 300;};  
-class vil_SVU_A: d_tit {buy = 8000;sell = 2000;}; 
-class vil_SVU: d_tit {buy = 8000;sell = 2000;}; 
-class vil_SVD_63: d_tit {buy = 10000;sell = 5000;}; 
-class vil_SVD_N: d_tit {buy = 12000;sell = 3000;}; 
-class vil_SVD_S: d_tit {buy = 12000;sell = 3000;}; 
-class vil_SVD_M: d_tit {buy = 12000;sell = 3000;}; 
-class vil_SVD_P21: d_tit {buy = 12000;sell = 3000;}; 
-class vil_SVDK: d_tit {buy = 30000;sell = 5000;}; 
+class vil_SVU_A: d_tit {buy = 8000;sell = 1000;}; 
+class vil_SVU: d_tit {buy = 8000;sell = 1000;}; 
+class vil_SVD_63: d_tit {buy = 10000;sell = 3000;}; 
+class vil_SVD_N: d_tit {buy = 12000;sell = 2000;}; 
+class vil_SVD_S: d_tit {buy = 12000;sell = 2000;}; 
+class vil_SVD_M: d_tit {buy = 12000;sell = 2000;}; 
+class vil_SVD_P21: d_tit {buy = 12000;sell = 2000;}; 
+class vil_SVDK: d_tit {buy = 30000;sell = 2000;}; 
 class vil_SV_98: d_tit {buy = 7000;sell = 2000;}; 
 class vil_SV_98_69: d_tit {buy = 8000;sell = 2000;}; 
 class vil_SV_98_SD: d_tit {buy = 9000;sell = 2500;}; 
