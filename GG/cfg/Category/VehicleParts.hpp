4c933a667e1cf59aab88b92e1cd9bf1343d2bda7
GhostGamerz <Psychotic_closure@hotmail.com>
2016-04-25 13:36:12 -0700

    Piece of metal sells for piece of shit

--- GG/cfg/Category/VehicleParts.hpp
+++ GG/cfg/Category/VehicleParts.hpp
@@ -7,7 +7,7 @@ class PartEngine: d_tit {buy = 500;sell = 250;};
 class PartVRotor: d_tit {buy = 500;sell = 250;}; 
 class PartWheel: d_tit {buy = 200;sell = 100;}; 
 class PartGlass: d_tit {buy = 100;sell = 50;}; 
-class PartGeneric: d_tit {buy = 200;sell = 100;}; 
+class PartGeneric: d_tit {buy = 200;sell = 25;}; 
 class PartFueltank: d_tit {buy = 200;sell = 100;}; 
 class ItemFuelBarrel: d_tit {buy = 200;sell = 100;}; }; 
 
