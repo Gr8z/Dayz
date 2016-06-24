ceaa0f8124931c7b3d2a8862a717edbb5e430d91
BigEgg <jeremydupont17@outlook.com>
2016-06-23 21:52:08 -0500

    Adjusted Gem buy prices

--- GG/cfg/Category/Gems.hpp
+++ GG/cfg/Category/Gems.hpp
@@ -1,9 +1,9 @@
 
 class Category_1020 { 
-class ItemSapphire: d_tit {buy = 60000;sell = 5000;}; 
-class ItemEmerald: d_tit {buy = 10000;sell = 1000;}; 
-class ItemCitrine: d_tit {buy = 30000;sell = 2000;}; 
-class ItemTopaz: d_tit {buy = 20000;sell = 1000;}; 
-class ItemAmethyst: d_tit {buy = 40000;sell = 3000;}; 
-class ItemObsidian: d_tit {buy = 50000;sell = 4000;}; 
-class ItemRuby: d_tit {buy = 70000;sell = 6000;}; }; 
+class ItemSapphire: d_tit {buy = 5000;sell = 5000;}; 
+class ItemEmerald: d_tit {buy = 1000;sell = 1000;}; 
+class ItemCitrine: d_tit {buy = 2000;sell = 2000;}; 
+class ItemTopaz: d_tit {buy = 1000;sell = 1000;}; 
+class ItemAmethyst: d_tit {buy = 3000;sell = 3000;}; 
+class ItemObsidian: d_tit {buy = 4000;sell = 4000;}; 
+class ItemRuby: d_tit {buy = 6000;sell = 6000;}; }; 
