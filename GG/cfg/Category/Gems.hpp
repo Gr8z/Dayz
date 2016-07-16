23bdadec1c79f6794644f6e2b8f3378526118eb0
BigEgg <jeremydupont17@outlook.com>
2016-07-16 00:10:30 -0500

    Adjustments

--- GG/cfg/Category/Gems.hpp
+++ GG/cfg/Category/Gems.hpp
@@ -6,4 +6,4 @@ class ItemCitrine: d_tit {buy = 2000;sell = 2000;};
 class ItemTopaz: d_tit {buy = 1000;sell = 1000;}; 
 class ItemAmethyst: d_tit {buy = 3000;sell = 3000;}; 
 class ItemObsidian: d_tit {buy = 4000;sell = 4000;}; 
-class ItemRuby: d_tit {buy = 15000;sell = 15000;}; }; 
+class ItemRuby: d_tit {buy = 11000;sell = 11000;}; }; 
